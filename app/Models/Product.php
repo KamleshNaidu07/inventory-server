96d9142e25744e6d6c40b180fc2c75653196bac0
Kamlesh Naidu <kamleshnaidu07@gmail.com>
2024-03-05 12:40:33 +0530

    added swagger schema to products0

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,68 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *     description="Product model",
+ *     title="Product",
+ *     @OA\Property(
+ *         property="id",
+ *         description="ID of the product",
+ *         type="integer",
+ *         example=1,
+ *     ),
+ *     @OA\Property(
+ *         property="name",
+ *         description="Name of the product",
+ *         type="string",
+ *         example="Example Product",
+ *     ),
+ *     @OA\Property(
+ *         property="description",
+ *         description="Description of the product",
+ *         type="string",
+ *         example="Example product description.",
+ *     ),
+ *     @OA\Property(
+ *         property="quantity",
+ *         description="Quantity of the product",
+ *         type="integer",
+ *         example=10,
+ *     ),
+ *     @OA\Property(
+ *         property="price",
+ *         description="Price of the product",
+ *         type="number",
+ *         format="float",
+ *         example=19.99,
+ *     ),
+ *     @OA\Property(
+ *         property="category_id",
+ *         description="ID of the associated category",
+ *         type="integer",
+ *         example=1,
+ *     ),
+ *     @OA\Property(
+ *         property="supplier_id",
+ *         description="ID of the associated supplier",
+ *         type="integer",
+ *         example=1,
+ *     ),
+ *     @OA\Property(
+ *         property="image_url",
+ *         description="URL of the product's image",
+ *         type="string",
+ *         example="https://example.com/product_image.jpg",
+ *     ),
+ *     @OA\Property(
+ *         property="price_formatted",
+ *         description="Formatted price of the product with two decimal places",
+ *         type="string",
+ *         readOnly=true,
+ *         example="19.99",
+ *     ),
+ * )
+ */
 class Product extends Model
 {
     use HasFactory;
